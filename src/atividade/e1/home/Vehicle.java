e447300494ef6091edf2f11755459b54d316dd53
Lucas Cesar <atomo.lambert_0q@icloud.com>
2022-08-30 21:14:45 -0300

    initial

---
+++ src/atividade/e1/home/Vehicle.java
@@ -0,0 +1,49 @@
+package atividade.e1.home;
+
+public class Vehicle {
+    // Variables
+    private String carColor;
+    private String licensePlate;
+    private String carType;
+    private String carModel;
+
+
+    // Acess methods
+    public String getCarColor() {
+        return carColor;
+    }
+
+    public String getCarModel() {
+        return carModel;
+    }
+
+    public String getCarType() {
+        return carType;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setCarColor(String carColor) {
+        this.carColor = carColor;
+    }
+
+    public void setCarModel(String carModel) {
+        this.carModel = carModel;
+    }
+
+    public void setCarType(String carType) {
+        this.carType = carType;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+
+
+}
